3af4c1b2c6c07c9d6f9bd1f6763afcaebb44ad1f
jaidesx <ssalijamal697@gmail.com>
2025-07-02 23:27:41 +0300

    modified the blog card and removed the bugs .I also work on the icons on various blogs

--- src/pages/blogs/components/BlogCard.tsx
+++ src/pages/blogs/components/BlogCard.tsx
@@ -28,11 +28,21 @@ export default function BlogCard({blog} : BlogCardProps) {
                 <h2 className={styles.blogName}>{blog.name}</h2>
                 <p className={styles.blogSummary}>{getSummary(blog.body)}</p>
                 <div className={styles.blogMeta}>
-                    <span className={styles.blogIcon}><CalendarDays /> {blog.publicationDate}</span>
-                    <span className={styles.blogIcon}><Star />{blog.rating} (127 reviews)</span>
-                    <span className={styles.blogIcon}><MessageCircleMore /> {blog.numComments} Comments</span>
+                    <span className={styles.blogPublicationDate} ><CalendarDays className={`${styles.blogIcon} ${styles.dateIcon}`}  />{new Date(blog.publicationDate).toLocaleDateString('en-US', {
+                        month: 'short',
+                        day: 'numeric',
+                        year: 'numeric'
+                    })}</span>
+                    <span className={styles.blogRating} >
+                    <Star className={`${styles.blogIcon} ${styles.ratingIcon}`} />
+                    {blog.rating} Rating
+                    </span>
+                    <span className={styles.blogNumComments} >
+                        <MessageCircleMore className={`${styles.blogIcon} ${styles.commentsIcon}`} />
+                        {blog.numComments} Comments
+                    </span>
                     <Link to={`/blogs/${blog.id}`}>
-                        <button className={styles.blogReadMoreButton}>Read more <MoveRight /></button>
+                        <button className={styles.blogReadMoreButton}>Read More <MoveRight /></button>
                     </Link>
                 </div>
             </div>
